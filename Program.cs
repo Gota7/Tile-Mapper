29907a2edc17d650b5b8c25bda3accdd4a3ccb60
Gota7 <gotaseven@gmail.com>
2022-10-02 21:19:47 -0400

    Add initial files

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ImGuiNET;
+using Raylib_cs;
+using rlImGui_cs;
+
+namespace TileMapper
+{
+    static class Program
+    {
+
+        // This is a demo program. See the following:
+        // ImGui.NET: https://github.com/mellinoe/ImGui.NET
+        // Raylib-cs: https://github.com/ChrisDill/Raylib-cs
+        // rlImGui-cs: https://github.com/raylib-extras/rlImGui-cs
+        public static void Main(string[] args)
+        {
+            Raylib.SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_VSYNC_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE);
+            Raylib.InitWindow(1280, 800, "raylib-Extras-cs [ImGui] example - simple ImGui Demo");
+            Raylib.SetTargetFPS(144);
+
+            rlImGui.Setup(true);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGRAY);
+
+                rlImGui.Begin();
+                ImGui.ShowDemoWindow();
+                rlImGui.End();
+
+                Raylib.EndDrawing();
+            }
+
+            rlImGui.Shutdown();
+            Raylib.CloseWindow();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Tile-Mapper
 Utility for mapping tiles.
+
+## Coding Style
+See the [Microsoft C# Coding Conventions](https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions).
+
+## Contributing
+1. Open an issue, if not already existing.
+2. Create a new branch with the issue number and your github name (Ex: 7-Gota7).
+3. Implement changes to your branch and necessary tests (if applicable).
+4. Submit a Pull Request to merge your changes into master.
+5. Your changes will be reviewed and merged.
