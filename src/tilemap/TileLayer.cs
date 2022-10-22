b10146530e188f3dc8ed5cb547246a94a02c2a4f
Popcom48 <cjwilson2k@gmail.com>
2022-10-22 16:06:07 -0400

    Resolved C# convention issues and removed unimplemented Draw() methods from TileLayer and Tile Map

--- src/tilemap/TileLayer.cs
+++ src/tilemap/TileLayer.cs
@@ -1,72 +1,72 @@
 ﻿using System.Threading.Tasks.Dataflow;
 
-namespace Tile_Mapper.tilemap
+namespace TileMapper
 {
-    //Class to store and manipulate information related to tile layers
+    // Class to store and manipulate information related to tile .
     public class TileLayer
     {
-        //Variables
+        // Variables.
 
-        private string tileSet;
-        private int[,] tilePlacements;
+        private string _tileSet;
+        private int[,] _tilePlacements;
 
-        //Constructor for new layer
+        // Constructor for new layer.
         public TileLayer(uint rows, uint columns, string tileSet)
         {
-            this.tileSet = tileSet;
-            this.tilePlacements = new int[rows,columns];
+            this._tileSet = tileSet;
+            this._tilePlacements = new int[rows,columns];
 
-            //Initialize a blank layer
+            // Initialize a blank layer.
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    this.tilePlacements[i,j] = -1;
+                    this._tilePlacements[i,j] = -1;
                 }
             }
         }
 
-        //Constructor for loaded layer
+        // Constructor for loaded layer.
         public TileLayer(int[,] tiles, string tileSet)
         {
-            this.tileSet = tileSet;
-            this.tilePlacements = tiles;
+            this._tileSet = tileSet;
+            this._tilePlacements = tiles;
         }
 
-        //Method to get tile at location
+        // Method to get tile at location.
         public int GetTile(uint x, uint y)
         {
-            return this.tilePlacements[x,y];
+            return this._tilePlacements[x,y];
         }
 
-        //Method to set tile at location
+        // Method to set tile at location.
         public void SetTile(uint x, uint y, int newId)
         {
-            this.tilePlacements[x, y] = newId;
+            this._tilePlacements[x, y] = newId;
         }
 
-        //Method to change the tile set for the layer
+        // Method to change the tile set for the layer.
         public void SetTileSet(string tileSet)
         {
-            this.tileSet = tileSet;
+            this._tileSet = tileSet;
         }
 
-        //Method to resize the layer
+        // Method to resize the layer.
         public void Resize(uint rows, uint columns)
         {
             int[,] newTiles = new int[rows,columns];
 
-            int oldRows = this.tilePlacements.GetLength(0);
-            int oldCols = this.tilePlacements.GetLength(1);
+            int oldRows = this._tilePlacements.GetLength(0);
+            int oldCols = this._tilePlacements.GetLength(1);
 
-            //Migrate current Layer
+            // Migrate current Layer.
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
                     if(i < oldRows && j < oldCols)
                     {
-                        newTiles[i, j] = this.tilePlacements[i, j];
+                        newTiles[i, j] = this._tilePlacements[i, j];
                     }
                     else
                     {
@@ -75,13 +75,7 @@ namespace Tile_Mapper.tilemap
                 }
             }
 
-            this.tilePlacements = newTiles;
-        }
-
-        //Method to draw the layer at a location
-        public void Draw(float x, float y, float scale)
-        {
-            throw new NotImplementedException();
+            this._tilePlacements = newTiles;
         }
     }
 }
--- src/tilemap/TileMap.cs
+++ src/tilemap/TileMap.cs
@@ -1,134 +1,128 @@
-﻿namespace Tile_Mapper.tilemap
+﻿namespace TileMapper
 {
-    //Class to store and manipulate information related to current tile map
+    // Class to store and manipulate information related to current tile map.
     public class TileMap
     {
-        //Variables
+        // Variables.
 
-        string filePath;
+        private string _filePath;
 
-        private ushort rows;
-        private ushort columns;
+        private ushort _rows;
+        private ushort _columns;
 
-        private ushort tileWidth;
-        private ushort tileHeight;
+        private ushort _tileWidth;
+        private ushort _tileHeight;
 
-        private List<TileLayer> layers;
-        private int currentLayer;
+        private List<TileLayer> _layers;
+        private int _currentLayer;
 
-        //Constructor for new map
+        // Constructor for new map.
         public TileMap()
         {
-            this.filePath = "";
-            this.currentLayer = -1;
+            this._filePath = "";
+            this._currentLayer = -1;
 
-            this.rows = 100;
-            this.columns = 100;
+            this._rows = 100;
+            this._columns = 100;
         }
 
-        //Constructor that loads from file
+        // Constructor that loads from file.
         public TileMap(string filePath)
         {
-            this.filePath = filePath;
+            this._filePath = filePath;
 
             throw new NotImplementedException();
         }
 
-        //Method to retrieve a layer for a given index
+        // Method to retrieve a layer for a given index.
         public TileLayer GetLayer(int index)
         {
-            return index >= this.layers.Count ? null : this.layers[index];
+            return index >= this._layers.Count ? null : this._layers[index];
         }
 
-        //Method to get the current layer
+        // Method to get the current layer.
         public TileLayer GetCurrentLayer()
         {
-            return this.layers[this.currentLayer];
+            return this._layers[this._currentLayer];
         }
 
-        //Method to change the current layer by index
+        // Method to change the current layer by index.
         public void SetCurrentLayer(int layerIndex)
         {
-            this.currentLayer = layerIndex;
+            this._currentLayer = layerIndex;
         }
 
-        //Method to add a new layer to the map
+        // Method to add a new layer to the map.
         public TileLayer AddLayer(string tileSet)
         {
-            TileLayer newLayer = new TileLayer(rows,columns,tileSet);
+            TileLayer newLayer = new TileLayer(this._rows, this._columns,tileSet);
 
-            this.layers.Add(newLayer);
-            this.currentLayer = layers.Count - 1;
+            this._layers.Add(newLayer);
+            this._currentLayer = this._layers.Count - 1;
 
             return newLayer;
         }
 
-        //Method to remove layer by index
+        // Method to remove layer by index.
         public void DeleteLayer(int index)
         {
-            this.layers.RemoveAt(index);
+            this._layers.RemoveAt(index);
         }
 
-        //Method to remove layer by class
+        // Method to remove layer by class.
         public void DeleteLayer(TileLayer layer)
         {
-            this.layers.Remove(layer);
+            this._layers.Remove(layer);
         }
 
-        //Method to swap the index of two layers
+        // Method to swap the index of two layers.
         public void SwapLayers(int firstIndex, int secondIndex)
         {
-            //Ensuring the two layers are valid
-            if(firstIndex < this.layers.Count && secondIndex < this.layers.Count)
+            // Ensuring the two layers are valid.
+            if(firstIndex < this._layers.Count && secondIndex < this._layers.Count)
             {
-                TileLayer temp = this.layers[firstIndex];
-                this.layers[firstIndex] = this.layers[secondIndex];
-                this.layers[secondIndex] = temp;
+                TileLayer temp = this._layers[firstIndex];
+                this._layers[firstIndex] = this._layers[secondIndex];
+                this._layers[secondIndex] = temp;
             }
         }
 
-        //Method to return the number of layers
+        // Method to return the number of layers.
         public int GetLayerCount()
         {
-            return this.layers.Count;
+            return this._layers.Count;
         }
 
-        //Method to resize the numbers of rows and columns
+        // Method to resize the numbers of rows and columns.
         public void Resize(ushort newRows, ushort newCol)
         {
-            this.rows = newRows;
-            this.columns = newCol;
+            this._rows = newRows;
+            this._columns = newCol;
 
-            //Resize each layer
-            foreach(TileLayer layer in this.layers)
+            // Resize each layer.
+            foreach(TileLayer layer in this._layers)
             {
                 layer.Resize(newRows,newCol);
             }
         }
 
-        //Method to resize tile pixels
+        // Method to resize tile pixels.
         public void SetUnitSize(ushort newWidth, ushort newHeight)
         {
-            this.tileWidth = newWidth;
-            this.tileHeight = newHeight;
+            this._tileWidth = newWidth;
+            this._tileHeight = newHeight;
         }
 
-        //Method to save current TileMap to a new file path
+        // Method to save current TileMap to a new file path.
         public void Save(string filePath)
         {
-            this.filePath = filePath;
+            this._filePath = filePath;
 
             this.Save();
         }
 
-        //Method to save to stored file path
+        // Method to save to stored file path.
         public void Save()
-        {
-            throw new NotImplementedException();
-        }
-
-        //Method to draw the map at a given position
-        public void Draw(float x, float y, float scale)
         {
             throw new NotImplementedException();
         }
