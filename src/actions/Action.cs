8834aec85fd9a9c581527f0072748758440fab7c
Popcom48 <cjwilson2k@gmail.com>
2022-11-28 22:05:06 -0500

    Made interfaces for generic actions and circular queue to undo/redo them. No implementations yet.

---
+++ src/actions/Action.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Tile_Mapper
+{
+    // Interface to genericise generating an edit object for an action.
+    public interface Action
+    {
+        // Method to generate an edit object for an action.
+        public EditAction GenetateAction();
+    }
+}
---
+++ src/actions/place action/PlaceAction.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Tile_Mapper
+{
+    // Action to place a single tile at a location.
+    internal class PlaceAction : Action
+    {
+
+        public EditAction GenetateAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
